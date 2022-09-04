871354917dcb6477a35db77fa9a0109242d972d7
Paulo Henrique Silva <paulohsilva2001@gmail.com>
2022-09-03 21:49:19 -0300

    Criando arquivo de configuração de mapper  das entidades

---
+++ MeAgendaAe.CrossCutting/Config/MapperConfig.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MeAgendaAe.CamadaDados.Tabelas;
+using MeAgendaAe.Dominio.Dtos;
+using MeAgendaAe.Dominio.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeAgendaAe.CrossCutting.Config
+{
+    public class MapperConfig
+    {
+        public static MapperConfiguration RegistrarMaps()
+        {
+            var mappingsConfigs = new MapperConfiguration(config =>
+            {
+                config.CreateMap<TbAgendamentos, Agendamentos>().ReverseMap();
+                config.CreateMap<TbAgendamentos, DtoAgendamento>().ReverseMap();
+            });
+
+            return mappingsConfigs;
+        }
+    }
+}
